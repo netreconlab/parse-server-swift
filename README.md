f31c74d519002c5d2bc8dbe41104a42ccd734b0d
Corey Baker <coreyearleon@icloud.com>
2022-06-25 10:15:10 -0400

    update docs

--- README.md
+++ README.md
@@ -12,4 +12,13 @@ This lets you change app behavior on the fly and add new features faster.
 To configure, you should edit [ParseServerSwift/Sources/App/configure.swift](https://github.com/netreconlab/ParseServerSwift/blob/main/Sources/App/configure.swift)
 
 ### WebhookKey
-The `webhookKey` should match 
+The `webhookKey` should match the [webhookKey on the Parse Server](https://github.com/parse-community/parse-server/blob/42c954318926823446326c95188b844e19954711/src/Options/Definitions.js#L491-L494). If you decide not the a `webhookKey`, set the value to `nil` in your ParseServerSwift.
+
+### Hostname and Port
+By default, the hostname is `127.0.0.1` and the port is `8081`. These values can easily be changed:
+
+```swift
+app.http.server.configuration.hostname = "your.hostname.com"
+app.http.server.configuration.port = 8081
+app.http.server.configuration.tlsConfiguration = .none
+```
--- Sources/ParseServerSwift/configure.swift
+++ Sources/ParseServerSwift/configure.swift
@@ -3,10 +3,10 @@ import ParseSwift
 import Vapor
 
 /// The key used to authenticate incoming webhook calls from a Parse Server
-var webhookKey: String? = "webhookKey" // Change to match your Parse Server's webhookKey or comment out.
+let webhookKey: String? = "webhookKey" // Change to match your Parse Server's webhookKey or comment out.
 
 /// The current address of ParseServerSwift.
-var serverPathname: String?
+var serverPathname: String!
 
 // configures your application
 public func configure(_ app: Application) throws {
@@ -17,22 +17,24 @@ public func configure(_ app: Application) throws {
     // Change to your specific hostname or comment out to use default.
     // app.http.server.configuration.hostname = "4threconbn.cs.uky.edu"
     app.http.server.configuration.port = 8081
+    app.http.server.configuration.tlsConfiguration = .none
     serverPathname = app.http.server.configuration.buildServerURL()
+
+    // Parse uses tailored encoders/decoders. These can be retreived from any ParseObject.
     ContentConfiguration.global.use(encoder: User.getJSONEncoder(), for: .json)
     ContentConfiguration.global.use(decoder: User.getDecoder(), for: .json)
 
+    // Required: Change to your Parse Server serverURL.
     guard let parseServerUrl = URL(string: "http://localhost:1337/1") else {
         throw ParseError(code: .unknownError,
                          message: "Could not make Parse Server URL")
     }
 
     // Initialize the Parse-Swift SDK
-    ParseSwift.initialize(applicationId: "applicationId", // Change to your applicationId
-                          clientKey: "clientKey", // Change to your clientKey
-                          masterKey: "masterKey", // Change to your masterKey
-                          serverURL: parseServerUrl, // Change to your serverURL
-                          allowingCustomObjectIds: false,
-                          usingEqualQueryConstraint: false) { _, completionHandler in
+    ParseSwift.initialize(applicationId: "applicationId", // Required: Change to your applicationId.
+                          clientKey: "clientKey", // Required: Change to your clientKey.
+                          masterKey: "masterKey", // Required: Change to your masterKey.
+                          serverURL: parseServerUrl) { _, completionHandler in
         completionHandler(.performDefaultHandling, nil)
     }
 
@@ -41,7 +43,7 @@ public func configure(_ app: Application) throws {
             let parseHealth = try await ParseHealth.check()
             app.logger.notice("Parse Server (\(parseServerUrl.absoluteString)) health is \"\(parseHealth)\"")
         } catch {
-            app.logger.error("Could not connect to Parse Server (\(parseServerUrl.absoluteString)): \(error)")
+            app.logger.error("Could not connect to Parse Server (\(parseServerUrl)): \(error)")
         }
     }
 
