7c589d552b46b174fa64ef0cf0a4367d4a21bc8e
Corey Baker <coreyearleon@icloud.com>
2023-04-04 00:04:46 -0400

    fix: Update to ParseSwift 5.4.1

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "4.75.0")),
         .package(url: "https://github.com/netreconlab/Parse-Swift.git",
-                 .upToNextMajor(from: "5.4.0")),
+                 .upToNextMajor(from: "5.4.1")),
     ],
     targets: [
         .target(
