87ce55bdad35003830aea29200b9df4e47ed8576
Corey Baker <coreyearleon@icloud.com>
2023-09-24 18:28:38 -0400

    fix file

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
             .library(name: "ParseServerSwift", targets: ["ParseServerSwift"])
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", exact: "4.83.2"),
+        .package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "4.83.2")),
         .package(url: "https://github.com/netreconlab/Parse-Swift.git",
                  .upToNextMajor(from: "5.8.1"))
     ],
