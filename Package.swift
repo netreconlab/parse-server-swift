f3a4f444b8886c0b47b277cb4df69382954a214b
Corey Baker <coreyearleon@icloud.com>
2026-07-28 13:09:07 -0700

    refactor: Update to latest vapor version

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/vapor/vapor.git",
-			.upToNextMajor(from: "4.121.4")
+			.upToNextMajor(from: "4.122.0")
 
         ),
         .package(
